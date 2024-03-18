73e32fcec058b9486e20bd99067975235f3d7daa
rs333 <9875430+rs333@users.noreply.github.com>
2024-03-17 21:13:45 -0400

    Working on data types section.

---
+++ src/bin/types.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("A simple rust program.");
+    let mut a: i8 = 42;
+
+    while a != 41 {
+        a = a.wrapping_add(1);
+
+        println!("a = {a}, a/2 = {},  a % 5 = {}", a / 2, a % 5);
+    }
+    a = a / 2;
+    println!("a/2 = {a}");
+
+    let tup: (i32, f64, u8, char) = (500, 6.4, 25, 'a');
+    println!("Print a tuple? {},{},{},{}", tup.0, tup.1, tup.2, tup.3);
+
+    let pairs = [(1, 'a'), (2, 's'), (3, 'd')];
+    let dog: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("Pairs: {} ", pairs[0].1);
+}
